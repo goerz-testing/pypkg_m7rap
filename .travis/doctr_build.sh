93a246f316c4b5b84f5e458ce2007308ea02ee25
Michael Goerz <goerz@stanford.edu>
2019-12-28 14:47:03 -0500

    Refactor doctr_build.sh for local testing

--- .travis/doctr_build.sh
+++ .travis/doctr_build.sh
@@ -1,14 +1,19 @@
 # This script is called from travis.yml for the "Docs" job
+#
+# Actual deployment will only happen if the TRAVIS environment variable is set.
+# It is possible to run this script locally (outside of Travis) for a test of
+# artifact building by setting only the TRAVIS_TAG variable, e.g.:
+#
+#   TRAVIS_TAG=v1.0.0-rc1 sh .travis/doctr_build.sh
+#
+# This will leave artifacts in the docs/_build/artifacts folder.
+
 echo "# DOCTR - deploy documentation"
 
 echo "## Generate main html documentation"
 tox -e docs
 
-if [[ -z "$TRAVIS_TAG" ]]; then
-
-    echo "Deploying as BRANCH $TRAVIS_BRANCH"
-
-else
+if [ ! -z "$TRAVIS_TAG" ]; then
 
     echo "Deploying as TAG $TRAVIS_TAG"
 
@@ -25,12 +30,12 @@ else
     # will then later transfer those links into versions.json
 
     echo "### [zip]"
-    cp -r docs/_build/html docs/pypkg_m7rap-$TRAVIS_TAG
-    cd docs/ || exit
+    cp -r docs/_build/html docs/_build/pypkg_m7rap-$TRAVIS_TAG
+    cd docs/_build || exit
     zip -r "pypkg_m7rap-$TRAVIS_TAG.zip" "pypkg_m7rap-$TRAVIS_TAG"
     rm -rf "pypkg_m7rap-$TRAVIS_TAG"
-    cd ../ || exit
-    mv docs/*.zip docs/_build/artifacts
+    cd ../../ || exit
+    mv "docs/_build/pypkg_m7rap-$TRAVIS_TAG.zip" docs/_build/artifacts
 
     echo "### [pdf]"
     tox -e docs -- -b latex _build/latex
@@ -42,46 +47,63 @@ else
     tox -e docs -- -b epub _build/epub
     mv docs/_build/epub/*.epub "docs/_build/artifacts/pypkg_m7rap-$TRAVIS_TAG.epub"
 
-    # upload to bintray
-    # Depends on $BINTRAY_USER, $BINTRAY_REPO, $BINTRAY_PACKAGE, and secret $BINTRAY_TOKEN from .travis.yml
-    echo "Upload artifacts to bintray"
-    for filename in docs/_build/artifacts/*; do
-        BINTRAY_UPLOAD="https://api.bintray.com/content/$BINTRAY_USER/$BINTRAY_REPO/$BINTRAY_PACKAGE/$TRAVIS_TAG/$(basename $filename)"
-        echo "Uploading $filename artifact to $BINTRAY_UPLOAD"
-        response=$(curl -T "$filename" "-u$BINTRAY_USER:$BINTRAY_TOKEN" "$BINTRAY_UPLOAD")
-        if [ -z "${response##*success*}" ]; then
-            echo "Uploaded $filename: $response"
-            echo "https://dl.bintray.com/$BINTRAY_USER/$BINTRAY_REPO/$(basename $filename)" >> docs/_build/html/_downloads
+    if [ ! -z "$TRAVIS" ]; then
+
+        # upload to bintray
+        # Depends on $BINTRAY_USER, $BINTRAY_REPO, $BINTRAY_PACKAGE, and secret $BINTRAY_TOKEN from .travis.yml
+        echo "Upload artifacts to bintray"
+        for filename in docs/_build/artifacts/*; do
+            BINTRAY_UPLOAD="https://api.bintray.com/content/$BINTRAY_USER/$BINTRAY_REPO/$BINTRAY_PACKAGE/$TRAVIS_TAG/$(basename $filename)"
+            echo "Uploading $filename artifact to $BINTRAY_UPLOAD"
+            response=$(curl -T "$filename" "-u$BINTRAY_USER:$BINTRAY_TOKEN" "$BINTRAY_UPLOAD")
+            if [ -z "${response##*success*}" ]; then
+                echo "Uploaded $filename: $response"
+                echo "https://dl.bintray.com/$BINTRAY_USER/$BINTRAY_REPO/$(basename $filename)" >> docs/_build/html/_downloads
+            else
+                echo "Error: Failed to upload $filename: $response" && sync && exit 1
+            fi
+        done
+        echo "Publishing release on bintray"
+        BINTRAY_RELEASE="https://api.bintray.com/content/$BINTRAY_USER/$BINTRAY_REPO/$BINTRAY_PACKAGE/$TRAVIS_TAG/publish"
+        response=$(curl -X POST "-u$BINTRAY_USER:$BINTRAY_TOKEN" "$BINTRAY_RELEASE")
+        if [ -z "${response##*files*}" ]; then
+            echo "Finished bintray release : $response"
         else
-            echo "Error: Failed to upload $filename: $response" && sync && exit 1
+            echo "Error: Failed publish release on bintray: $response" && sync && exit 1
         fi
-    done
-    echo "Publishing release on bintray"
-    BINTRAY_RELEASE="https://api.bintray.com/content/$BINTRAY_USER/$BINTRAY_REPO/$BINTRAY_PACKAGE/$TRAVIS_TAG/publish"
-    response=$(curl -X POST "-u$BINTRAY_USER:$BINTRAY_TOKEN" "$BINTRAY_RELEASE")
-    if [ -z "${response##*files*}" ]; then
-        echo "Finished bintray release : $response"
-    else
-        echo "Error: Failed publish release on bintray: $response" && sync && exit 1
+
+        echo "docs/_build/html/_downloads:"
+        cat docs/_build/html/_downloads
+
+        rm -rf docs/_build/artifacts
+
     fi
 
-    echo "docs/_build/html/_downloads:"
-    cat docs/_build/html/_downloads
+elif [ ! -z "$TRAVIS_BRANCH" ]; then
 
-    rm -rf docs/_build/artifacts
+    echo "Deploying as BRANCH $TRAVIS_BRANCH"
+
+else
+
+    echo "At least one of TRAVIS_TAG and TRAVIS_BRANCH must be set"
+    sync
+    exit 1
 
 fi
 
 # Deploy
-echo "## pip install doctr"
-python -m pip install doctr
-echo "## doctr deploy"
-if [[ -z "$TRAVIS_TAG" ]]; then
-    DEPLOY_DIR="$TRAVIS_BRANCH"
-else
-    DEPLOY_DIR="$TRAVIS_TAG"
+if [ ! -z "$TRAVIS" ]; then
+    echo "## pip install doctr"
+    python -m pip install doctr
+    echo "## doctr deploy"
+    if [ ! -z "$TRAVIS_TAG" ]; then
+        DEPLOY_DIR="$TRAVIS_TAG"
+    else
+        DEPLOY_DIR="$TRAVIS_BRANCH"
+    fi
+    python -m doctr deploy --key-path docs/doctr_deploy_key.enc \
+        --command="git show $TRAVIS_COMMIT:.travis/doctr_post_process.py > post_process.py && git show $TRAVIS_COMMIT:.travis/versions.py > versions.py && python post_process.py" \
+        --built-docs docs/_build/html --no-require-master --build-tags "$DEPLOY_DIR"
 fi
-python -m doctr deploy --key-path docs/doctr_deploy_key.enc \
-    --command="git show $TRAVIS_COMMIT:.travis/doctr_post_process.py > post_process.py && git show $TRAVIS_COMMIT:.travis/versions.py > versions.py && python post_process.py" \
-    --built-docs docs/_build/html --no-require-master --build-tags "$DEPLOY_DIR"
+
 echo "# DOCTR - DONE"
