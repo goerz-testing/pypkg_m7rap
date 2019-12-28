609879797669130df1b9f9e48f5382c29f589767
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2019-12-28 20:40:50 +0000

    Update docs after building Travis build 10 of goerz/pypkg_m7rap
    The docs were built from the branch 'master' against the commit
    bb27985255e3d390d7ecbdd9cec454e85b6ccaee.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/goerz/pypkg_m7rap/jobs/630427978.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.8.0/lib/python3.8/site-packages/doctr/__main__.py deploy --key-path docs/doctr_deploy_key.enc '--command=git show bb27985255e3d390d7ecbdd9cec454e85b6ccaee:.travis/doctr_post_process.py > post_process.py && git show bb27985255e3d390d7ecbdd9cec454e85b6ccaee:.travis/versions.py > versions.py && python post_process.py' --built-docs docs/_build/html --no-require-master --build-tags master

--- master/_static/documentation_options.js
+++ master/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.0-dev (93a246f)',
+    VERSION: '0.1.0-dev (bb27985)',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
